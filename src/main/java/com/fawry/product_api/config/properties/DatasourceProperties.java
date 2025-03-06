1c4065da8c0f7787c0d7f519e47ccc5596fd3a36
Hossam Hatem <7ossam7atem1@gmail.com>
2025-03-06 11:17:46 +0200

    feat(db):add configuration properties for database settings

--- src/main/java/com/fawry/product_api/config/properties/DatasourceProperties.java
+++ src/main/java/com/fawry/product_api/config/properties/DatasourceProperties.java
@@ -1,8 +1,17 @@
 package com.fawry.product_api.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "custom.datasource")
-public class DatasourceProperties {}
+@Getter
+@Setter
+public class DatasourceProperties {
+    private String driver;
+    private String url;
+    private String username;
+    private String password;
+}
